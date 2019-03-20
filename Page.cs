2fa806ff075dfbd662614aaa47f111e09056bf5c
hopefully-purple <48627093+hopefully-purple@users.noreply.github.com>
2019-03-20 11:15:38 -0600

    retrying

--- Page.cs
+++ Page.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 public class Page : System.Web.UI.TemplateControl, System.Web.IHttpHandler 
 {
 
-   // Define a handler for the button click.
-    protected void SubmitBtn_Click(object sender, EventArgs e)
-    {	
+//    // Define a handler for the button click.
+//     protected void SubmitBtn_Click(object sender, EventArgs e)
+//     {	
 
-        MySpan.InnerHtml = "Hello, " + MyTextBox.Text + ".";
+//         MySpan.InnerHtml = "Hello, " + MyTextBox.Text + ".";
 
-    }
+//     }
 
 }
